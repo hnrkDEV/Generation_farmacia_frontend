f1cb084a96787dd6ac48e48084361b00e38bc45a
João Henrique <jhenrique.caval@hotmail.com>
2026-02-25 14:55:50 -0300

    navbar e footer concluídos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element />
+            {/*         <Route path="/home" element={<Home />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastrartema" element={<FormTema />} />
+            <Route path="/editartema/:id" element={<FormTema />} />
+            <Route path="/deletartema/:id" element={<DeletarTema />} /> */}
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
